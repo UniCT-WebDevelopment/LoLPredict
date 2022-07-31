a54b44ad7e01322e594e01d4d1f03c35bd88dc42
alaendro <alessandro2aprile@gmail.com>
2022-07-31 23:49:47 +0200

    aggiunti commenti su cosa fare

---
+++ public/assets/js/frontend.js
@@ -0,0 +1,5 @@
+//per ottenere informazioni del player tocca capire prima come prendere quei dati stampati dal backend.js
+
+//TODO fare il #data_player dove prende dati dati per ora d default
+
+//risultati machine learning appena si ha machine learning
