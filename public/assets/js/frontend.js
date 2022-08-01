b264b273cbef461cac39d8e3e8b6ce24d435b269
alaendro <alessandro2aprile@gmail.com>
2022-08-01 22:44:49 +0200

    ajax frontend funzionante

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -6,19 +6,21 @@
 
 //https://stackoverflow.com/questions/5618827/ajaxy-add-parameters-to-request
 
+
 $.ajax({
-    url: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext",
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: {
-        XRiotToken: "RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9"
-    },
-    origin: "*",
-    type: "POST",
+    url: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext?api_key=RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
+    type: "GET",
     datatype: "json",
+    //'X-Riot-Token': "RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
+    /*headers: {
+        //'Access-Control-Allow-Headers': "*",
+        
+        //'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+    }*/
 })
 .done(function(json){
+    console.log("ci sei riuscito");
+    console.log(json);
     $( "<h1>" ).text( json.title ).appendTo( "#data_player");
     $( "<div>").html( json.html ).appendTo( "#data_player");
 })
@@ -27,3 +29,33 @@ $.ajax({
     console.log( "Error: " + errorThrown );
     console.log( "Status: " + status );
 });   
+
+
+
+//let link = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext";
+
+
+/*
+
+async function getSummonerRank() {
+    let link = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext"+"?api_key=RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9";
+    
+    const response = await fetch(link, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            "Access-Control-Allow-Methods":"GET, POST, DELETE, PUT",
+            "Access-Control-Allow-Headers":"X-Requested-With, Content-Type, Authorization, Origin, Accept"
+        }
+    }); 
+    
+    let data = await response.json();
+    return data;
+}
+
+console.log(getSummonerRank());
+
+*/
