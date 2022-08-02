9c6259d66cc653ca82af09e5f5c0edde78c12dde
alaendro <alessandro2aprile@gmail.com>
2022-08-02 19:21:12 +0200

    modifiche minori

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -1,5 +1,4 @@
 //per ottenere informazioni del player tocca capire prima come prendere quei dati stampati dal backend.js
-
 //TODO fare il #data_player dove prende dati dati per ora d default
 
 //risultati machine learning appena si ha machine learning
@@ -9,16 +8,36 @@
 
 
 //valutare se fare queste call in backend e poi il backend passa solo i dati che servono
-//sistemare le chiamate con i parametri e mettere la key una variabile così che sia facilmente accessibile se cambia
-//let key_api;
+let key_api = "RGAPI-83d1f1dc-4f5d-4c4f-b41a-0867b828bef6";
+
+/*
+async function get_data_from_name(){
+    fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext?api_key=" + key_api, {
+    method: "GET",
+    headers:{
+        //'Access-Control-Allow-Origin': "Accept",
+        'Access-Control-Request-Method': "GET",
+        //'Access-Control-Allow-Headers': "Accept"
+    }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data;
+    })
+    .catch((error) => {
+        console.log("error: ", error);
+    });
+}
+*/
 
 
-async function get_data_from_name(){ //aggiungere poi come parametro "name" che viene dato dal backend
+async function get_data_from_name(summoner_name){ //aggiungere poi come parametro "name" che viene dato dal backend
+    let url_req = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner_name + "?api_key=";
     $.ajax({
-        url: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/AlexNext?api_key=RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
+        url: url_req + key_api,
         type: "GET",
         datatype: "json",
-        //'X-Riot-Token': "RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
         /*headers: {
             //'Access-Control-Allow-Headers': "*",
             
@@ -28,7 +47,6 @@ async function get_data_from_name(){ //aggiungere poi come parametro "name" che 
     .done(function(json){
         console.log(json);
         return json;
-
         //console.log("roba", user_info_json.puuid);
     })
     .fail(function( xhr, status, errorThrown ) {
@@ -38,9 +56,11 @@ async function get_data_from_name(){ //aggiungere poi come parametro "name" che 
     });
 }
 
-async function get_matches_from_puuid(){
+async function get_matches_from_puuid(puuid_player){
+    let url_req = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid_player + "/ids?start=0&count=50&api_key=";
+    //https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/Yv1Ql3_Hf0O_8iiZ_rV5etYEnIAZyAEacPupj2KGQuRTnwedTCOdECWA68ifHe-LABKxJOrlU_V-vg/ids?start=0&count=50&api_key="
     $.ajax({
-        url: "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/Yv1Ql3_Hf0O_8iiZ_rV5etYEnIAZyAEacPupj2KGQuRTnwedTCOdECWA68ifHe-LABKxJOrlU_V-vg/ids?start=0&count=50&api_key=RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
+        url: url_req + key_api,
         type: "GET",
         datatype: "json",
         //'X-Riot-Token': "RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
@@ -66,7 +86,7 @@ async function get_matches_from_puuid(){
 
 async function get_info_match(){
     $.ajax({
-        url: "https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5995849487?api_key=RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
+        url: "https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_5995849487?api_key=" + key_api,
         type: "GET",
         datatype: "json",
         //'X-Riot-Token': "RGAPI-86459c0f-49f2-448e-b5c7-e11cdb91c1a9",
@@ -87,13 +107,18 @@ async function get_info_match(){
     });   
 }
 
+
 //non funziona se metti await, a caso ma dà le cose in ordine con i console.log dentro ajax
 //non si applica la stessa cosa, come dovrebbe essere, con i console log dei valori di ritorno
 
+get_data_from_name("AlexNext").then((json) => {
+    get_matches_from_puuid(json.puuid);
+})
 
-let user_info_json = await get_data_from_name();
-let set_puuid = await get_matches_from_puuid();
-let info_match = await get_info_match();
+/*
+let user_info_json = get_data_from_name();
+let set_puuid =  get_matches_from_puuid();
+let info_match =  get_info_match();
 
 console.log(user_info_json);
 console.log(set_puuid);
