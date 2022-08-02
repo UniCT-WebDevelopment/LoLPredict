09ffef809cf21cfe08f459a9708e6c818e7f9f15
FrancescoDiBella <francescino10401@gmail.com>
2022-08-02 19:51:19 +0200

    Added Electron and renderer.js file

---
+++ renderer.js
@@ -0,0 +1,11 @@
+const { ipcRenderer } = require("electron")
+
+window.addEventListener("DOMContentLoaded", () => {
+    const el = {
+        documentName : document.getElementById("documentName"),
+        createDocumentBtn: document.getElementById("createDocumentBtn"),
+        fileTextArea : document.getElementById("fileTextarea"),
+        openDocumentBtn : document.getElementById("openDocumentBtn")
+    }
+
+})
