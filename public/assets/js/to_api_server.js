446713fbe32369e1b4cb56eb888525c692c24aea
alaendro <alessandro2aprile@gmail.com>
2022-08-04 19:00:19 +0200

    Added get_winrate_player_champions and improved performance

--- public/assets/js/to_api_server.js
+++ public/assets/js/to_api_server.js
@@ -7,7 +7,7 @@
 
 
 //valutare se fare queste call in backend e poi il backend passa solo i dati che servono
-let key_api = "RGAPI-84c05a14-1945-41db-a906-0460b66f708e";
+let key_api = "RGAPI-af8be21a-00e3-4c80-90ea-2504da46efa5";
 
 //funzione definiva?
 let summoner_id_player;
@@ -69,6 +69,7 @@ function analize_matches(data, num_games){
         //console.log(name_game[i]);
     }
 
+    let sum_games = 0;
 
     for(let j = 0; j < num_games; j++){
 
@@ -81,30 +82,153 @@ function analize_matches(data, num_games){
         })
         .then(response => response.json())
         .then(data => {
-            console.log("eseguendo fetch n" + j);
+            //console.log("eseguendo fetch n" + j);
+            sum_games++;
             for(let y = 0; y < 10; y++){
-                console.log("ciclo n " + y + " di richiesta fetch numero " + j);
+                //console.log("ciclo n " + y + " di richiesta fetch numero " + j);
                 //console.log(data.info.participants[y]);
                 if(data.info.participants[y].puuid == puuid_player){ 
                     if(data.info.participants[y].win == true){
                         games_winned++;
-                        console.log(games_winned);
+                        //console.log(games_winned);
                     }
                     break;
                 }
             }  
         })
         .then(() => {
-            winrate_player = (games_winned/num_games)*100;
-            console.log("winrate player " + winrate_player);
-            console.log("num_games " + num_games);
-            console.log("games_winned " + games_winned);
+            if(sum_games == num_games){
+                winrate_player = (games_winned/num_games)*100;
+                console.log("winrate player " + winrate_player);
+                console.log("num_games " + num_games);
+                console.log("games_winned " + games_winned);
+            }
         })
         if((j % 15) == 0){
             sleep(2000); //2000 va bene e non dà troppi rallentamenti se non si tocca la funzione sleep
         }
 
         //console.log("finito ciclo " + j);
+    }
+}
+
+let champions_player = new Array();
+
+class champion{
+    winrate;
+    games_played;
+    games_winned;
+    champion_name;
+    constructor(champion_name){
+        this.champion_name = champion_name;
+        this.winrate = 0;
+        this.games_played = 0;
+        this.games_winned = 0;
+    }
+    increase_games_played(){
+        this.games_played++;
+    }
+    increase_games_winned(){
+        this.games_winned++;
+    }
+    calculate_winrate(){
+        this.winrate = (this.games_winned/this.games_played)*100;
+    }
+    check_champion_presence(name){
+        if(this.champion_name == name)
+            return true;
+        else
+            return false;
+    }
+    get_info(){
+        console.log("champion " + this.champion_name);
+        console.log("winrate " + this.winrate);
+        console.log("games played " + this.games_played);
+        console.log("games winned " + this.games_winned);
+    }
+}
+
+function analize_matches_champions(data, num_games){
+    let last_games_played = JSON.stringify(JSON.stringify(data));
+    let first_half_url = "https://europe.api.riotgames.com/lol/match/v5/matches/";
+    let second_half_url = "?api_key=" + key_api;
+
+    let name_game = last_games_played.split(',');
+    
+    for(let i = 0; i < num_games; i++){
+        //console.log(name_game[i]);
+        if(i == 0){
+            name_game[i] = name_game[i].substr(4, 15);
+        }
+        else if(i == num_games-1){
+            name_game[i] = name_game[i].substr(2, 15);
+        }
+        else{
+            name_game[i] = name_game[i].substr(2, 15);
+        }
+        //console.log(name_game[i]);
+    }
+
+    let sum_games = 0;
+
+    for(let j = 0; j < num_games; j++){
+        let complete_url = first_half_url + name_game[j] + second_half_url;
+        fetch(complete_url, {
+            method: "GET",
+            headers:{
+                'Access-Control-Request-Method': "GET",
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            //console.log("eseguendo fetch n" + j);
+            sum_games++;
+            for(let y = 0; y < 10; y++){
+                //console.log("ciclo n " + y + " di richiesta fetch numero " + j);
+                //console.log(data.info.participants[y]);
+                if(data.info.participants[y].puuid == puuid_player){ 
+                    if(data.info.participants[y].win == true){
+                        games_winned++;
+                    }
+                    let presence = false;
+                    //console.log(data.info.participants[y].championName);
+                    for(let z = 0; z < champions_player.length; z++){
+                        if(champions_player[z].check_champion_presence(data.info.participants[y].championName) == true){
+                            presence = true;
+                            champions_player[z].increase_games_played();
+                            if(data.info.participants[y].win == true){
+                                champions_player[z].increase_games_winned();
+                            }
+                            break;
+                        }
+                    }
+                    if(presence == false){
+                        let add_champ = new champion(data.info.participants[y].championName);
+                        if(data.info.participants[y].win == true){
+                            add_champ.increase_games_winned();
+                        }
+                        add_champ.increase_games_played();
+                        champions_player.push(add_champ);
+                    }
+                    break;
+                }
+            }  
+        })
+        .then(() => {
+            if(sum_games == num_games){
+                winrate_player = (games_winned/num_games)*100;
+                for(let i = 0; i < champions_player.length; i++){
+                    champions_player[i].calculate_winrate();
+                    champions_player[i].get_info();
+                }
+                console.log("winrate player " + winrate_player);
+                console.log("num_games " + num_games);
+                console.log("games_winned " + games_winned);
+            }
+        })
+        if((j % 15) == 0){
+            sleep(2000); 
+        }
     }
 }
 
@@ -120,7 +244,20 @@ function get_winrate_player(summoner_name, num_games){
     })
 }
 
-get_winrate_player("AlexNext", 50); 
+function get_winrate_player_champions(summoner_name, num_games){
+    get_info_summoner_name(summoner_name)
+    .then(response => response.json())
+    .then(data => {
+        get_list_matches(data.puuid, num_games)
+        .then(response => response.json())
+        .then(data => {
+            analize_matches_champions(data, num_games);
+        })
+    })
+}
+
+//get_winrate_player("AlexNext", 40);
+get_winrate_player_champions("AlexNext", 40);
 
 /* //prova per vedere se funzionava 
 function d(){
