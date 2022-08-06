ca702338c3d60ccfbad5856ca5ba5538d46249dd
alaendro <alessandro2aprile@gmail.com>
2022-08-06 14:05:44 +0200

    aggiunti metodi per futuro machine learning e commenti vari

--- public/assets/js/to_api_server.js
+++ public/assets/js/to_api_server.js
@@ -14,8 +14,11 @@ let summoner_id_player;
 let puuid_player;
 let winrate_player;
 let games_winned = 0;
-let games_played;
+let games_played = 0;
 let last_champion_played;
+let winrate_againts_champion;
+let games_againts_champion = 0;
+let win_againts_champion = 0;
 
 function sleep(miliseconds) {
     var currentTime = new Date().getTime();
@@ -105,6 +108,17 @@ function analize_matches(data, num_games){
                 console.log("games_winned " + games_winned);
             }
         })
+        .catch(() =>{
+            num_error++;
+            if((num_error+sum_games) == num_games){ //così da fare il calcolo solo una volta
+                winrate_player = (games_winned/sum_games)*100;
+                console.log("winrate player " + winrate_player);
+                console.log("num_games " + sum_games);
+                console.log("games_winned " + games_winned);
+
+                console.log("Fetch error: " + num_error);
+            }
+        })
         if((j % 15) == 0){
             sleep(2000); //2000 va bene e non dà troppi rallentamenti se non si tocca la funzione sleep
         }
@@ -142,14 +156,14 @@ class champion{
             return false;
     }
     get_info(){
-        console.log("champion " + this.champion_name);
-        console.log("winrate " + this.winrate);
-        console.log("games played " + this.games_played);
-        console.log("games winned " + this.games_winned);
+        console.log("Champion: " + this.champion_name);
+        console.log("Winrate with " + this.champion_name + ": " + this.winrate);
+        console.log("Games played with" + this.champion_name + ": " + this.games_played);
+        console.log("Games winned with" + this.champion_name + ": " + this.games_winned);
     }
 }
 
-function analize_matches_champions(data, num_games){
+function analize_matches_champions(data, num_games, champion_againts){
     let last_games_played = JSON.stringify(JSON.stringify(data));
     let first_half_url = "https://europe.api.riotgames.com/lol/match/v5/matches/";
     let second_half_url = "?api_key=" + key_api;
@@ -185,10 +199,21 @@ function analize_matches_champions(data, num_games){
         .then(data => {
             //console.log("eseguendo fetch n" + j);
             sum_games++;
+            let teamid_againts;
+            let teamid_player;
+            let num_player;
+            //console.log("champion againts " + champion_againts);
             for(let y = 0; y < 10; y++){
                 //console.log("ciclo n " + y + " di richiesta fetch numero " + j);
                 //console.log(data.info.participants[y]);
-                if(data.info.participants[y].puuid == puuid_player){ 
+                if(data.info.participants[y].championName == champion_againts && data.info.participants[y].puuid != puuid_player){
+                    games_againts_champion++;
+                    teamid_againts = data.info.participants[y].teamId;
+                    num_player = y;
+                    console.log("campione trovato", games_againts_champion, teamid_againts, num_player);
+                }
+                if(data.info.participants[y].puuid == puuid_player){
+                    teamid_player = data.info.participants[y].teamId;
                     if(data.info.participants[y].win == true){
                         games_winned++;
                     }
@@ -212,12 +237,28 @@ function analize_matches_champions(data, num_games){
                         add_champ.increase_games_played();
                         champions_player.push(add_champ);
                     }
+                    if(champion_againts == undefined)
+                        break;
+                    else{
+                        continue;
+                    }
+                }
+                if(teamid_againts != undefined && teamid_player != undefined){
+                    console.log("dentro ultimo if");
+                    if(teamid_againts == teamid_player){
+                        games_againts_champion--;
+                    }
+                    else{
+                        if(data.info.participants[num_player].win != true){
+                            win_againts_champion++;
+                        }
+                    }
                     break;
                 }
             }  
         })
         .then(() => {
-            if(sum_games == num_games){
+            if(sum_games == num_games){ //così da fare il calcolo solo una volta
                 winrate_player = (games_winned/num_games)*100;
                 for(let i = 0; i < champions_player.length; i++){
                     champions_player[i].calculate_winrate();
@@ -226,11 +267,15 @@ function analize_matches_champions(data, num_games){
                 console.log("winrate player " + winrate_player);
                 console.log("num_games " + num_games);
                 console.log("games_winned " + games_winned);
+
+                console.log(win_againts_champion, games_againts_champion);
+                winrate_againts_champion = (win_againts_champion/games_againts_champion)*100; 
+                console.log("Winrate againts " + champion_againts + " is: " + winrate_againts_champion);
             }
         })
         .catch(() =>{
             num_error++;
-            if((num_error+sum_games) == num_games){
+            if((num_error+sum_games) == num_games){ //così da fare il calcolo solo una volta
                 winrate_player = (games_winned/sum_games)*100;
                 for(let i = 0; i < champions_player.length; i++){
                     champions_player[i].calculate_winrate();
@@ -239,6 +284,11 @@ function analize_matches_champions(data, num_games){
                 console.log("winrate player " + winrate_player);
                 console.log("num_games " + sum_games);
                 console.log("games_winned " + games_winned);
+
+                winrate_againts_champion = (win_againts_champion/games_againts_champion)*100;
+                console.log("Winrate againts " + champion_againts + " is " + winrate_againts_champion);
+
+                console.log("Fetch error: " + num_error);
             }
         })
         if((j % 15) == 0){
@@ -299,14 +349,14 @@ function get_winrate_player(summoner_name, num_games){
     })
 }
 
-function get_winrate_player_champions(summoner_name, num_games){
+function get_winrate_player_champions(summoner_name, num_games, champion_againts = undefined){
     get_info_summoner_name(summoner_name)
     .then(response => response.json())
     .then(data => {
         get_list_matches(data.puuid, num_games)
         .then(response => response.json())
         .then(data => {
-            analize_matches_champions(data, num_games);
+            analize_matches_champions(data, num_games, champion_againts);
         })
     })
 }
@@ -320,25 +370,34 @@ function get_last_champion_played(summoner_name){
 }
 
 //get_winrate_player("AlexNext", 40);
-//get_winrate_player_champions("AlexNext", 40); //da usare per mostare le informazioni nel riquadro
+//get_winrate_player_champions("AlexNext", 40); //da usare per mostare le informazioni sia nel riquadro che per il machine learning
+//get_winrate_player_champions("AlexNext", 10, "Olaf"); //winrate contro il cammpione con cui è
+//si deve eseguire lo stesso codice anche per avere solo l'informazione per lo specifico campione e si scriverebbe una funzione uguale quindi meglio averne una sola così che viene fatta partire una sola volta per il player avversario
 //get_last_champion_played("AlexNext"); //utile per mettere immagine del campione nella grafica
 
+//appunto momentaneo, fare nel main.js l'ultimo punto della riga 400, forse meglio fare prima il file json?
 
 
+//PER CICCIO, INVECE DI RENDERE QUESTO FILE UN MODULO LO SI FA COMUNQUE ESEGUIRE COSì COM'è MA I DATI VENGONO SALVATI SU UN JSON A CUI IL MAIN ACCEDE
+//VANTAGGI: NIENTE CAZZI NEL GESTIRE MODULI, FUNZIONI ETC VISTO CHE è TUTTO IN QUESTO FILE, RENDERE MODULO QUESTO FILE NON è POSSIBILE PERCHè NON è UN NODE FILE, TUTTI I DATI SONO SALVATI E NON SI FANNO ALTRE FETCH
+//SVANTAGGI: NESSUNO, PERCHè I CONSOLE LOG VANNO POI GESTITI NEL JSON FIN DA SUBITO
+//FORSE UNO SVANTAGGIO è GESTIRE LA LETTURA DAL FILE JSON MA NEMMENO COSì TANTO VISTO CHE C'è IL MODULO ADATTO
 
 //fixare bug dove se chiudi il launcher ti dà il nickname (feature?)
 
-//spostare questo file in main.js facendo diventare modulo? vedere se si può fare tutto sennò si hanno problemi con la fetch
+//spostare questo file in main.js facendo diventare modulo? vedere se si può fare tutto sennò si hanno problemi con la fetch //probabilmente non si fa
 
 //completare main.js per prendere informazioni      -quali, quelli per il machine learning? le uniche che ormai servono     
 //-per alessandro
 
 //to_api_server.js passa le informazioni al render.js che lui li cambia     -è fattibile?
 
+//creare file json per memorizzare i record per i player che si loggano così da non fare troppe richieste all'api, quindi creare metodi analoghi (più semplici da fare)
+
 //metodo per prendere dati player da dare al tensorflow    
 // -per alessandro?
 
-//dati da prendere - verosimilmente winrate player, winrate con quel champion, winrate contro quello contro cui è, differenza di rank nella partita sia tra la squadre che tra il giocatore e l'avversario
+//dati da prendere - verosimilmente winrate players, winrate con quel champion, winrate contro quello contro cui è (per entrambi i player), differenza di rank nella partita sia tra la squadre che tra il giocatore e l'avversario
 //come salvare modello di tensorflow
 
 
